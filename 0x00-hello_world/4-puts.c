7b30e63179dca4dc0c68c962454c0cf170be4bdf
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-07-13 06:16:01 -0700

    task 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints using puts
- * Return: 0 (success)
+ * main - Entry point program that uses puts function
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
