4375173c9019f0f0b1bd11ee104d6986a326c713
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-07-14 03:35:12 -0700

    task 10  program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,13 @@ int main(void)
 {
 int num1;
 int num2;
-for (num1 = '0'; num1 < '9'; num1++)
+for (num1 = 0; num1 < 9; num1++)
 {
-for (num2 = num1 + 1; num2 <= '9'; num2++)
+for (num2 = num1 + 1; num2 < 10; num2++)
 {
-if (num2 != num1)
-{
-putchar(num1);
-putchar(num2);
-while (num1 == '8' && num2 == '9')
+putchar('0' + num1);
+putchar('0' + num2);
+if (num1 == 8 && num2 == 9)
 continue;
 putchar(',');
 putchar(' ');
