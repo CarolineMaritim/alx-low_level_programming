9b361fcb151966c9bbdab411f44a237b8ebbb87d
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-07-14 02:00:50 -0700

    task 4 alphabet lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - pogram the prints alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ *
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; letter <= 'z'; letter++)
+{
+if (alphabet != 'e' && alphabet != 'q')
+	putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
