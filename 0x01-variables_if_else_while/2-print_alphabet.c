3647b39e56ee28da0aa4626a1c830ff7aa69c296
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-07-14 01:28:03 -0700

    task 2 using put char to to print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stlib.h>
+/**
+ * main - pogram the prints alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ *
+*/
+int main(void)
+{
+char ch;
+putchar(tolower(ch));
+return (0);
+}
