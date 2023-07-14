d94d76bbb625901ba46545bf4b84b5b17bb5c83d
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-07-14 01:39:22 -0700

    task 3 to lower to upper case using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - pogram the prints alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ *
+*/
+int main(void)
+{
+char ch;
+putchar(tolower(ch));
+putchar(toupper(ch));
+putchar('\n');
+return (0);
+}
