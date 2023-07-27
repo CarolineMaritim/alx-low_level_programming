d7d84128fc55aa55e5dc4d3eb67c9b886dde0610
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-07-27 04:19:09 -0700

    string concatanatinf func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-while (*dest)
-dest++;
-while (*src)
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
