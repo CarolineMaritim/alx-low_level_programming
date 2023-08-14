07153fdebb49011a19be919e8dd0559daef96982
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-08-14 02:38:54 -0700

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Describes dog
+ * @name: Name
+ * @age: It's age
+ * @owner: It's owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
