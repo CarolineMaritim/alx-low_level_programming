3129f88c7891635103a002b3fbd6167f4b37a511
Caroline Chepkoech <carolynamaritmi@gmail.com>
2023-08-16 01:58:34 -0700

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Iterates a function in an array
+ * @array: Array checked
+ * @size: size of the aray
+ * @action: Pointer to the function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+do
+{
+action(*array);
+array++;
+}
+while (size-- > 0);
+}
