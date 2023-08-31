0f18c937e3783750c94410aeb7f68ab675c9b71f
CarolineMaritim <carolynamaritim@gmail.com>
2023-08-31 06:29:17 -0700

    Endinness check

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness - Checks endianness if little or big
+ * Return: 0 if big, else 1
+ */
+int get_endianness(void)
+{
+int bit = 1;
+char *e = (char *)&bit;
+if (*e == 1)/*if lS byte is 1 then it's a little endian*/
+return (1);
+return (0);
+}
