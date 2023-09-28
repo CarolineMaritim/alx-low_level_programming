b32627b15aae25179cb757b503ce3cde9044e50e
CarolineMaritim <carolynamaritim@gmail.com>
2023-09-28 12:44:45 +0300

    Bit manipulation and reverse engineering

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_binary - Prints binary rep of a num
+ * @n: Long interger checked
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		/*using recurssiion*/
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');/*convert to character*/
+}
+
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * set_bit - Sets value it to 1 at a given index
+ * @n: The number being checked
+ * @index: The index checked
+ *
+ * Return: 1 if success , else -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(n) * 8))/*checking for errors*/
+		return (-1);
+	*n ^= (1 << index);/*bitwise operand XOR*/
+	return (1);
+}
+
